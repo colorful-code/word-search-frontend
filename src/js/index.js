878ba7eb8c6d6b489615c46fd5681575204e48a6
Amir H <amir@colorfulcode.se>
2023-02-07 09:57:47 +0100

    Forget to switch back to real url after local testing. New dist.

--- src/js/index.js
+++ src/js/index.js
@@ -99,7 +99,9 @@ newGridBtn.addEventListener("click", async () => {
 
 async function fetchGrid(grid) {
   const commaSeperatedWords = grid.words.join(",");
-  const response = await fetch(`http://localhost:8080/wordgrid?gridSize=${grid.size}&words=${commaSeperatedWords}`);
+  const response = await fetch(
+    `https://word-search-api.onrender.com/wordgrid?gridSize=${grid.size}&words=${commaSeperatedWords}`
+  );
   const result = await response.json();
   if (result[1]) {
     const omittedWords = result[1].split(",");
